0aa0fafad4ed63425a7440777d070f613f82d54c
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2023-02-17 11:54:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TESLO API
+
+1. Levantar la base de datos
+
+``` docker compose up -d ```
+# teslo-shop-nest-js
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,41 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @IsOptional()
+    title?: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsString()
+    slug?: string;
+
+    @IsNumber()
+    @IsOptional()
+    @IsPositive()
+    @IsInt()
+    stock?: number;
+
+    @IsString({
+        each: true
+    })
+    @IsArray()
+    sizes:  string[];
+
+    @IsIn(['men', 'women', 'kid', 'unisex'])
+    gender: string;
+
+    @IsString({
+        each: true
+    })
+    @IsOptional()
+    tags?: string[];
+}
